c4e4c4d7ade266a1fbad5cbc94f1e4269493a060
Stephen DeBaun <sdebaun74@gmail.com>
2024-10-21 12:39:54 -0700

    last cleanup i swear lol

--- web/routes/app/_middleware.ts
+++ web/routes/app/_middleware.ts
@@ -1,11 +1,7 @@
-import { FreshContext, type Handler } from "$fresh/server.ts";
+import { FreshContext } from "$fresh/server.ts";
 import { getCookies } from "$std/http/cookie.ts";
 
-interface State {
-  data: string;
-}
-
-const redirectWithoutUser = (req: Request, ctx: FreshContext<State>) => {
+const redirectWithoutUser = (req: Request, ctx: FreshContext) => {
   // if no auth cookie, redirect
   const cookies = getCookies(req.headers);
   console.log(cookies);
@@ -19,30 +15,8 @@ const redirectWithoutUser = (req: Request, ctx: FreshContext<State>) => {
     });
   }
   return ctx.next();
-  // if it's an htmx request...
-  // if (req.headers.get('hx-request')) {
-  //   // headers.set("location", "/");
-  //   return new Response(null, {
-  //     status: 200,
-  //     headers,
-  //   });
-  // } else {
-  //   return new Response(null, {
-  //     status: 303,
-  //     headers
-  //   })
-  // }
-  // otherwise
 };
-// export async function handler(
-//   req: Request,
-//   ctx: FreshContext<State>,
-// ) {
-//   // ctx.state.data = "myData";
-//   const resp = await ctx.next();
-//   // resp.headers.set("server", "fresh server");
-//   return resp;
-// }
+
 export const handler = [
   redirectWithoutUser,
 ];
